29d7bbe629bb8cfd3ed04421b4a400302fdab7c3
Miqueas Martinez <miqueas2020@yahoo.com>
2021-01-17 18:47:44 -0500

    Transparent Header Bar

--- moonterm.lua
+++ moonterm.lua
@@ -15,6 +15,8 @@ lgi					= require("lgi")
 Gtk					= lgi.require('Gtk', '3.0')
 Vte					= lgi.Vte
 GLib				= lgi.GLib
+Hdy         = lgi.require("Handy", "1")
+Hdy.init()
 
 app					= Gtk.Application()
 term				= Vte.Terminal()
@@ -27,22 +29,34 @@ conf				= inifile:load(('%s/moonterm.ini'):format(dir))
 require('moonterm-dialog')
 
 scroll				= Gtk.ScrolledWindow()
-main_window	= Gtk.Window {
+main_window	= Hdy.Window {
 	width_request	= 600,
-	height_request	= 400,
-	scroll
+	height_request	= 400
 }
 
-headerbar    = Gtk.HeaderBar {
+styles = Gtk.CssProvider();
+styles:load_from_path("Styles.css");
+
+main_window:get_style_context().add_provider_for_screen(
+  main_window:get_screen(),
+  styles,
+  Gtk.STYLE_PROVIDER_PRIORITY_USER
+)
+
+headerbar    = Hdy.HeaderBar {
 	title 	 = 'MoonTerm',
-	subtitle = 'a simple vte terminal in lua',
 	show_close_button = true,
-    Gtk.Button {
+	valign = Gtk.Align.START,
+
+  Gtk.Button {
 		visible = true,
 		on_clicked = function () dialog_config:show_all() end,
 		Gtk.Image({icon_name = "gtk-justify-fill"})
-    }
+  }
 }
+
+headerbar:get_style_context():add_class("Header")
+term:get_style_context():add_class("Terminal")
 
 function term:on_child_exited()
 	app:quit()
@@ -62,8 +76,17 @@ function app:on_activate()
 		function() end
     )
 	dialog_config.child.entry_interpreter.text = conf.interpreter.executable
-	main_window:set_titlebar(headerbar)
+
 	scroll:add(term)
+
+	local over = Gtk.Overlay({
+		visible = true,
+
+		{ scroll, index = 0 }
+	})
+	over:add_overlay(headerbar)
+
+	main_window:add(over)
 	main_window.set_icon_name(main_window,'terminal')
 	main_window:show_all()
 	self:add_window(main_window)
