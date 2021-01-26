156d9f7581b725b68a705fb7654a20105061f11e
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-01-25 22:56:26 -0400

    fix focus

--- src/moonterm-popover.lua
+++ src/moonterm-popover.lua
@@ -12,14 +12,17 @@ popover_menu = Gtk.Popover({
 		Gtk.ModelButton {
 			id = 'btn_preferences',
 			label = "Preferences",
+			can_focus = false,
 			on_clicked = function () dialog_config:show_all() end,
 		},
 		Gtk.ModelButton {
 			label = "About Moonterm",
+			can_focus = false,
 			on_clicked = function () about_window:run() about_window:hide() end,
 		},
 		Gtk.Separator{},
 		Gtk.ModelButton {
+			can_focus = false,
 			label = "Quit",
 			on_clicked = function () app:quit() end,
 		},
