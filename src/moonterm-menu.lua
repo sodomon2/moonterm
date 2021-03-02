8dba02e57e25952f9149273799231dedfd58cc31
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-03-02 17:56:23 -0400

    Added menu `copy` and `paste`

--- src/moonterm-menu.lua
+++ src/moonterm-menu.lua
@@ -10,14 +10,24 @@ function main_window:on_button_press_event(event)
 	if (event.type == 'BUTTON_PRESS' and event.button == 3) then
 		menu = Gtk.Menu {
 			Gtk.ImageMenuItem {
-				label = "Preferences",
+				label = "Copy",
 				image = Gtk.Image {
-					stock = "gtk-preferences"
+					stock = "gtk-copy"
 				},
 				on_activate = function()
-					dialog_config:show_all()
+					term:copy_clipboard()
 				end
 			},
+			Gtk.ImageMenuItem {
+				label = "Paste",
+				image = Gtk.Image {
+					stock = "gtk-paste"
+				},
+				on_activate = function()
+					term:paste_clipboard()
+				end
+			},
+			Gtk.SeparatorMenuItem {},
 			Gtk.ImageMenuItem {
 				label = "About Moonterm",
 				image = Gtk.Image {
@@ -26,6 +36,15 @@ function main_window:on_button_press_event(event)
 				on_activate = function()
 					about_window:run()
 					about_window:hide()
+				end
+			},
+			Gtk.ImageMenuItem {
+				label = "Preferences",
+				image = Gtk.Image {
+					stock = "gtk-preferences"
+				},
+				on_activate = function()
+					dialog_config:show_all()
 				end
 			},
 			Gtk.SeparatorMenuItem {},
