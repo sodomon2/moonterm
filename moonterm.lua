dd2b3e1f808190009fce18eaca989e1b3c7f4ac3
sodomon2 <sodomon.dev@gmail.com>
2025-11-29 06:46:34 -0400

    Port to GTK4
    porting to gtk4 [WIP]
    
    editorconfig: indentation number reduced to 2

--- moonterm.lua
+++ moonterm.lua
@@ -8,17 +8,18 @@
  @date      16.01.2021 23:52:45 -04
 --]]
 
-shell 				= os.getenv("SHELL") or "/bin/sh"
-inifile				= require("libraries.LIP")
+shell 			= os.getenv("SHELL") or "/bin/sh"
+inifile			= require("libraries.LIP")
 utils				= require("libraries.utils")
 
-lgi					= require("lgi")
-Gtk					= lgi.require('Gtk', '3.0')
-Gdk					= lgi.require('Gdk', '3.0')
-Vte					= lgi.require('Vte', '2.91')
+local lgi					= require("lgi")
+Gtk					= lgi.require('Gtk', '4.0')
+Gdk					= lgi.require('Gdk', '4.0')
+Vte					= lgi.require('Vte', '3.91')
 GLib				= lgi.require('GLib', '2.0')
+Gio					= lgi.require('Gio', '2.0')
 
-app					= Gtk.Application()
+app 				= Gtk.Application.new("com.github.sodomon2.moonterm", Gio.ApplicationFlags.DEFAULT_FLAGS)
 term				= Vte.Terminal()
 
 utils:create_config('moonterm','moonterm.ini')
@@ -26,13 +27,13 @@ dir 				= ('%s/moonterm'):format(GLib.get_user_config_dir())
 conf				= inifile:load(('%s/moonterm.ini'):format(dir))
 
 if conf.moonterm.quake_mode == true then
-	Keybinder 		= lgi.require('Keybinder', '3.0')
+	Keybinder = lgi.require('Keybinder', '3.0')
 end
 
 -- MoonTerm
 require('src.moonterm-app')
-require('src.moonterm-menu')
-require('src.moonterm-dialog')
-require('src.moonterm-keybinds')
+-- require('src.moonterm-menu')
+-- require('src.moonterm-dialog')
+-- require('src.moonterm-keybinds')
 
 app:run()
